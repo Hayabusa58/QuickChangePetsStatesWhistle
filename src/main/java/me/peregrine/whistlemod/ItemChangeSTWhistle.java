67f28cec028c4165bd3b25b608863dea00d6f3de
Hayabusa <yuuta.keiko.k@live.jp>
2016-02-17 21:44:35 +0900

    - add ItemChangeSTWhistle.java - register ItemWhistle

---
+++ src/main/java/me/peregrine/whistlemod/ItemChangeSTWhistle.java
@@ -0,0 +1,7 @@
+package me.peregrine.whistlemod;
+
+import net.minecraft.item.ItemBow;
+
+public class ItemChangeSTWhistle extends ItemBow {
+
+}
--- src/main/java/me/peregrine/whistlemod/WhistlemodCore.java
+++ src/main/java/me/peregrine/whistlemod/WhistlemodCore.java
@@ -1,5 +1,52 @@
 package me.peregrine.whistlemod;
 
+import java.io.IOException;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.registry.GameRegistry;
+import net.minecraftforge.common.config.Configuration;
+
+@Mod(modid = "ChangeSTWhistle", name = "QuickChangeStatusWhistle")
 public class WhistlemodCore {
+	@Instance("ChangeSTWhitle")
+	public static WhistlemodCore instance;
+
+	//Logging
+	public static Logger logger = LogManager.getLogger("Whistlemod");
+
+	public static ItemChangeSTWhistle Whistle;
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event){
+		Whistle = new ItemChangeSTWhistle();
+		GameRegistry.registerItem(Whistle,"itemwhistle");
+
+	//Config
+	Configuration cfg = new Configuration(event.getSuggestedConfigurationFile());
+	try {
+		cfg.load();
+
+	} catch (Exception e) {
+		e.printStackTrace();
+	} finally {
+		cfg.save();
+	}
+
+
+
+	}
+
+	@EventHandler
+	public void postInit(FMLInitializationEvent event) throws IOException {
+	//レシピの登録等
+
+	}
+
 
 }
